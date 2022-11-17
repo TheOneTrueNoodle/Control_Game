5a6227f9aed12fe60f8b6c414d09b5076f404475
James Mohan <c20305011@mytudublin.ie>
2022-11-17 22:53:25 +0000

    Obstacle spawning is automatic

--- Control_Game_Project/Assets/Scripts/ObstacleSpawner.cs
+++ Control_Game_Project/Assets/Scripts/ObstacleSpawner.cs
@@ -4,20 +4,37 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    public GameObject[] obstacles;
-
+    [Header("Spawn Area")]
     public float spawnRangeX = 10;
     public float spawnRangeY = 10;
 
+    [Header("")]
+    public GameObject[] obstacles;
+
+    public float spawnRate;
+
+    private bool canSpawn = true;
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (canSpawn)
         {
-            int randomIndex = Random.Range(0, obstacles.Length);
-            Vector3 randomSpawnPosition = new Vector3(Random.Range(-spawnRangeX / 2, spawnRangeX / 2), Random.Range(-spawnRangeY / 2, spawnRangeY / 2), transform.position.z);
-
-            Instantiate(obstacles[randomIndex], randomSpawnPosition, Quaternion.identity);
+            StartCoroutine(Spawn());
         }
+    }
+
+    IEnumerator Spawn()
+    {
+        canSpawn = false;
+        float timeToNextSpawn = 1 / spawnRate;
+
+        int randomIndex = Random.Range(0, obstacles.Length);
+        Vector3 randomSpawnPosition = new Vector3(Random.Range(-spawnRangeX / 2, spawnRangeX / 2), Random.Range(-spawnRangeY / 2, spawnRangeY / 2), transform.position.z);
+
+        Instantiate(obstacles[randomIndex], randomSpawnPosition, Quaternion.identity);
+
+        yield return new WaitForSeconds(timeToNextSpawn);
+        canSpawn = true;
     }
 
     void OnDrawGizmosSelected()
